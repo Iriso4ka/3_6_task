ea7f8942670589f7f50381e0a621048b8b63ca6c
Iriso4ka <77170106+Iriso4ka@users.noreply.github.com>
2021-10-04 13:13:15 +0300

    Create test_items.py

---
+++ test_items.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+def test_button_cart_exist(browser):
+    browser.get(link)
+    assert browser.find_element(By.CLASS_NAME, "btn-add-to-basket"), 'корзина не найдена'
